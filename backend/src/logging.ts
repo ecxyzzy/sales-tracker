a1b7c35dc514101e4633c40ec657a549dd352027
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-07-03 14:29:20 -0700

    feat(backend): differentiate logging behavior

--- backend/src/logging.ts
+++ backend/src/logging.ts
@@ -1,10 +1,20 @@
+import 'dotenv/config';
 import pino from 'pino';
-export default pino({
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            translateTime: 'yyyy-mm-dd HH:MM:ss',
-        },
+
+const devSettings = {
+    level: 'debug', transport: {
+        target: 'pino-pretty', options: {
+            colorize: true, translateTime: 'SYS:standard'
+        }
     }
-});
+};
+
+const prodSettings = {
+    level: 'info', transport: {
+        target: 'pino/file', options: {
+            destination: process.env.LOG_PATH_PROD, mkdir: true
+        }
+    }
+};
+
+export default pino(process.env.NODE_ENV === 'development' ? devSettings : prodSettings);
