e883994f818af3ad98e83b963b2168691d99eb9f
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-07-03 13:09:21 -0700

    chore(backend): update eslintrc

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,11 +1,12 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
+    plugins: ['@typescript-eslint',],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended',],
+    rules: {
+        'quotes': ['error', 'single'],
+        'object-curly-spacing': ['error', 'always'],
+        'semi': ['error', 'always'],
+        'semi-style': ['error', 'last'],
+    }
 };
