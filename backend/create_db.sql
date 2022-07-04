417e629fcde7e946c98b9d83b06d0719765350ae
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-07-03 18:54:58 -0700

    fix(schema): standardize column names

--- backend/create_db.sql
+++ backend/create_db.sql
@@ -4,27 +4,27 @@ USE sales_tracker;
 CREATE TABLE users (
     uid SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    pswdhash CHAR(60) NOT NULL,
+    password CHAR(60) NOT NULL,
     is_admin BOOLEAN NOT NULL CHECK ((is_admin = 1 AND can_edit = 1) OR (is_admin = 0)),
     can_edit BOOLEAN NOT NULL
 );
 CREATE TABLE products (
     pid SERIAL PRIMARY KEY,
-    pname VARCHAR(255) NOT NULL
+    p_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE transactions (
     tid SERIAL PRIMARY KEY,
-    tdate DATE NOT NULL,
-    tproduct BIGINT UNSIGNED NOT NULL,
-    tcount DECIMAL(7,2) NOT NULL,
+    t_date DATE NOT NULL,
+    t_product BIGINT UNSIGNED NOT NULL,
+    t_count DECIMAL(7,2) NOT NULL,
     price DECIMAL(6,2) NOT NULL,
-    est_total DECIMAL(11,2) GENERATED ALWAYS AS (tcount * price),
+    est_total DECIMAL(11,2) GENERATED ALWAYS AS (t_count * price),
     act_total DECIMAL(11,2) NOT NULL,
     handler1 BIGINT UNSIGNED NOT NULL,
     handler2 BIGINT UNSIGNED,
     handler3 BIGINT UNSIGNED,
     remarks VARCHAR(255),
-    FOREIGN KEY (tproduct) REFERENCES products(pid),
+    FOREIGN KEY (t_product) REFERENCES products(pid),
     FOREIGN KEY (handler1) REFERENCES users(uid),
     FOREIGN KEY (handler2) REFERENCES users(uid),
     FOREIGN KEY (handler3) REFERENCES users(uid)
