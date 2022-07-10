270f2ae624841d03e0f36d3a3072b3d1c0437cb3
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-07-10 12:07:47 -0700

    fix(frontend): implement basic theming

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,15 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import appTheme from './themes/appTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={appTheme}>
+            <CssBaseline enableColorScheme />
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
---
+++ frontend/src/themes/appTheme.ts
@@ -0,0 +1,4 @@
+import { createTheme } from '@mui/material';
+
+const appTheme = createTheme();
+export default appTheme;
